136f2969116dd54418c3cd1c52d512db3e11fa35
Helloworld Park <helloworld.park@gmail.com>
2019-07-31 19:42:19 +0900

    First project setup

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  faster-hough-line-cpp
+//
+//  Created by 박성현 on 31/07/2019.
+//  Copyright © 2019 Sean Park. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
