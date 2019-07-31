cfecc21e0106b501f81c659ee0c8b92d6176ebf4
Helloworld Park <helloworld.park@gmail.com>
2019-07-31 21:21:54 +0900

    Hello World!

---
+++ src/FasterHough.cpp
@@ -0,0 +1,9 @@
+//
+//  FasterHough.cpp
+//  faster-hough-line-cpp
+//
+//  Created by 박성현 on 31/07/2019.
+//  Copyright © 2019 Sean Park. All rights reserved.
+//
+
+#include "FasterHough.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,25 @@
 //
 
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::string imgName("images/test0.jpg");
+    
+    cv::Mat image;
+    image = cv::imread(imgName, cv::IMREAD_COLOR);
+    
+    if (image.empty()) {
+        std::cout << "Failed to open image" << std::endl;
+        return -1;
+    }
+    
+    cv::namedWindow("Named");
+    cv::imshow("Named", image);
+    cv::waitKey(0);
+    
     return 0;
 }
